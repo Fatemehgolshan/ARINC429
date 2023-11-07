d073d9025f6b60692f60d39ec43744d159de70c2
fatemehgolshan <fag28@pitt.edu>
2023-11-07 00:10:27 -0500

    add new files

---
+++ uart/trasmitt_terminal/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace COM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboPort = new System.Windows.Forms.ComboBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtReceive = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnControl_Reg = new System.Windows.Forms.Button();
+            this.btnData_Fifo = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.conrol_register12k = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port:";
+            // 
+            // cboPort
+            // 
+            this.cboPort.FormattingEnabled = true;
+            this.cboPort.Location = new System.Drawing.Point(87, 47);
+            this.cboPort.Name = "cboPort";
+            this.cboPort.Size = new System.Drawing.Size(121, 21);
+            this.cboPort.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(214, 47);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(295, 47);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Send:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(87, 76);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(283, 115);
+            this.txtMessage.TabIndex = 5;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(416, 168);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.Location = new System.Drawing.Point(87, 214);
+            this.txtReceive.Multiline = true;
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReceive.Size = new System.Drawing.Size(283, 115);
+            this.txtReceive.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Receive:";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM9";
+            // 
+            // btnControl_Reg
+            // 
+            this.btnControl_Reg.Location = new System.Drawing.Point(376, 81);
+            this.btnControl_Reg.Name = "btnControl_Reg";
+            this.btnControl_Reg.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnControl_Reg.Size = new System.Drawing.Size(75, 23);
+            this.btnControl_Reg.TabIndex = 10;
+            this.btnControl_Reg.Text = "Control_Reg12.5Khz";
+            this.btnControl_Reg.UseVisualStyleBackColor = true;
+            this.btnControl_Reg.Click += new System.EventHandler(this.btnControl_Reg_Click);
+            // 
+            // btnData_Fifo
+            // 
+            this.btnData_Fifo.Location = new System.Drawing.Point(416, 122);
+            this.btnData_Fifo.Name = "btnData_Fifo";
+            this.btnData_Fifo.Size = new System.Drawing.Size(75, 23);
+            this.btnData_Fifo.TabIndex = 12;
+            this.btnData_Fifo.Text = "Data_Fifo";
+            this.btnData_Fifo.UseVisualStyleBackColor = true;
+            this.btnData_Fifo.Click += new System.EventHandler(this.btnData_Fifo_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // conrol_register12k
+            // 
+            this.conrol_register12k.Location = new System.Drawing.Point(457, 81);
+            this.conrol_register12k.Name = "conrol_register12k";
+            this.conrol_register12k.Size = new System.Drawing.Size(75, 23);
+            this.conrol_register12k.TabIndex = 14;
+            this.conrol_register12k.Text = "Control_Reg100K";
+            this.conrol_register12k.UseVisualStyleBackColor = true;
+            this.conrol_register12k.Click += new System.EventHandler(this.conrol_register12k_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(486, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "12.5khz";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(413, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "100khz";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(416, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(373, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Speed:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 411);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.conrol_register12k);
+            this.Controls.Add(this.btnData_Fifo);
+            this.Controls.Add(this.btnControl_Reg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtReceive);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.cboPort);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "terminal trans :)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboPort;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtReceive;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnControl_Reg;
+        private System.Windows.Forms.Button btnData_Fifo;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button conrol_register12k;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ uart/trasmitt_terminal/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace COM
+{
+    public partial class Form1 : Form
+    {
+        Thread rthread;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cboPort.Items.AddRange(ports);
+        //    if(ports.Length != 0)
+            cboPort.SelectedIndex = 0;
+            btnClose.Enabled = false;
+
+            serialPort1.Encoding = System.Text.Encoding.GetEncoding(28591);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+
+            btnOpen.Enabled = false;
+            btnClose.Enabled = true;
+            try
+            {
+                serialPort1.PortName = cboPort.Text;
+                serialPort1.Open();
+
+                rthread = new Thread(new ThreadStart(receiverThread));
+                rthread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    List<byte> buffer = new List<byte>();
+
+                    String[] aa = txtMessage.Text.Split(' ');
+
+                    foreach (String a in aa)
+                    {
+                        int value = Convert.ToInt32(a, 16);
+                        byte v = (byte)value;
+                        buffer.Add(v);
+                    }
+
+                    serialPort1.Write(buffer.ToArray(), 0, buffer.Count);
+                    //serialPort1.Write(ret);
+                    txtMessage.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = true;
+            btnClose.Enabled = false;
+            try
+            {
+                serialPort1.Close();
+                rthread.Abort();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            /*try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    txtReceive.Text = serialPort1.ReadExisting();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }*/
+        }
+
+        private void receiverThread()
+        {
+            string str = "";
+            while (true)
+            {
+                str = receive();
+                if (str != "")
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        txtReceive.Text = txtReceive.Text + str + "\r\n"; // runs on UI thread
+                    });
+                }
+            }
+        }
+
+        private string getHex(string val)
+        {
+            string ret = "";
+            foreach (char a in val)
+            {
+                string hex = BitConverter.ToString(new byte[] { Convert.ToByte(a) });
+                ret = ret + hex + " ";
+            }
+            return ret;
+        }
+
+        private string receive()
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    string val = serialPort1.ReadExisting();
+                    if (val != null)
+                    {
+                        return getHex(val);
+                    }
+                    else
+                    {
+                        return "";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return "";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                rthread.Abort();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String output = "BB";
+                if (serialPort1.IsOpen)
+                {
+                    List<byte> buffer = new List<byte>();
+
+                    String[] aa = output.Split(' ');
+
+                    foreach (String a in aa)
+                    {
+                        int value = Convert.ToInt32(a, 16);
+                        byte v = (byte)value;
+                        buffer.Add(v);
+                    }
+
+                    serialPort1.Write(buffer.ToArray(), 0, buffer.Count);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnControl_Reg_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String output = "CC 80";
+                if (serialPort1.IsOpen)
+                {
+                    List<byte> buffer = new List<byte>();
+
+                    String[] aa = output.Split(' ');
+
+                    foreach (String a in aa)
+                    {
+                        int value = Convert.ToInt32(a, 16);
+                        byte v = (byte)value;
+                        buffer.Add(v);
+                    }
+
+                    serialPort1.Write(buffer.ToArray(), 0, buffer.Count);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void conrol_register12k_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String output = "CC 40";
+                if (serialPort1.IsOpen)
+                {
+                    List<byte> buffer = new List<byte>();
+
+                    String[] aa = output.Split(' ');
+
+                    foreach (String a in aa)
+                    {
+                        int value = Convert.ToInt32(a, 16);
+                        byte v = (byte)value;
+                        buffer.Add(v);
+                    }
+
+                    serialPort1.Write(buffer.ToArray(), 0, buffer.Count);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnData_Fifo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String output = "AA";//
+                if (serialPort1.IsOpen)
+                {
+                    List<byte> buffer = new List<byte>();
+
+                    String[] aa = output.Split(' ');
+
+                    foreach (String a in aa)
+                    {
+                        int value = Convert.ToInt32(a, 16);
+                        byte v = (byte)value;
+                        buffer.Add(v);
+                    }
+
+                    serialPort1.Write(buffer.ToArray(), 0, buffer.Count);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtReceive.Text = "";
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
